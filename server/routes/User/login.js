7a233b6d17c73318d652abf1afd6551a8a9fd285
xuchen-imac <chenxu2656@gmail.com>
2022-04-11 19:06:42 +0800

    update

--- server/routes/User/login.js
+++ server/routes/User/login.js
@@ -1,12 +1,22 @@
 
-const bcrypt = require('bcrypt')
+const jwt=require("jsonwebtoken");
+
 const {Credentials} = require('../../model/User')
 module.exports = async(req,res)=>{
     let {email,password} = req.body
     try {
         const loginInfo = await Credentials(email,password)
+        const {username} = loginInfo
         if (loginInfo) {
-            res.status(200).json(loginInfo)
+            let secretkey=loginInfo.email;//加密字段
+            const token= jwt.sign(
+                {username},
+                secretkey,
+                {
+                    expiresIn:60*10
+                }
+            );
+            res.status(200).json(userInfo)
         } else {
             res.status(400).json({"err":"Wrong Credentials"})
         }
