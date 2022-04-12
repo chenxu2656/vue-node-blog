a8911cf65bd3f1289bb5021729b7cb3de97d5e59
xuchen-imac <chenxu2656@gmail.com>
2022-04-12 11:51:21 +0800

    update : get article list

--- server/model/article.js
+++ server/model/article.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const {articleStructure} = require('./schema')
 let articleSchema = new mongoose.Schema(articleStructure)
 const article = mongoose.model('article',articleSchema)
+// 创建文章
 const createArticle = async(title,content,tags)=>{
     const articleRes = await article.create({
         title: title,
@@ -10,7 +11,14 @@ const createArticle = async(title,content,tags)=>{
     })
     return articleRes
 }
+// 获取文章列表
+const getArticleList = async (req,res)=>{
+    // 
+    let articleList = await article.find()
+    return articleList
+}
 module.exports = {
     article: article,
-    createArticle: createArticle
+    createArticle: createArticle,
+    getArticleList: getArticleList
 }
--- server/routes/Article/articleList.js
+++ server/routes/Article/articleList.js
@@ -1,5 +1,14 @@
 // 获取所有的文章列表
-const {article} = require('../../model/article')
-module.exports = (req,res)=>{
-    
+const {getArticleList} = require('../../model/article')
+module.exports = async(req,res)=>{
+    const articleList  = await getArticleList()
+    console.log(articleList);
+    if(articleList.length > 0) {
+        res.status(200).json(articleList)
+    }else{
+        res.status(200).json({
+            'err': 'no article'
+        })
+    }
+
 }
