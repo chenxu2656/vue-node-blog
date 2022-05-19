74ef33b5bd084a0758c9ca6708b525d4f0b2d7d4
chenchen.xu <chenxu2656@163.com>
2022-05-19 22:48:20 +0800

    update

--- src/components/admin/content/HomePage.vue
+++ src/components/admin/content/HomePage.vue
@@ -1,16 +1,42 @@
 <template>
-  <el-row :gutter="20">
-    <el-col :span="6"><div class="grid-content bg-purple" />123</el-col>
-    <el-col :span="6"><div class="grid-content bg-purple-light" />123</el-col>
-    <el-col :span="6"><div class="grid-content bg-purple" />123</el-col>
-    <el-col :span="6"><div class="grid-content bg-purple" />123</el-col>
-  </el-row>
+  <div id="headBar">
+    <el-row :gutter="20">
+      <el-col :span="6">
+        <div class="grid-content bg-purple">
+          <div class="title"><span>博客<br>数量</span></div>
+        </div>
+      </el-col>
+      <el-col :span="6"><div class="grid-content bg-purple">
+        <div class="title"><span>评论<br>数量</span></div>
+        </div>
+      </el-col>
+      <el-col :span="6"><div class="grid-content bg-purple" /></el-col>
+      <el-col :span="6"><div class="grid-content bg-purple" /></el-col>
+    </el-row>
+  </div>
 </template>
 <style lang="scss" scoped>
-.el-row{
-  .el-col{
-    height: 100px;
-    background-color: aqua;
+#headBar {
+  margin-top: 20px;
+  .el-row {
+    .el-col {
+      .grid-content {
+        height: 100px;
+        background-color: #15b7b9;
+        border-radius: 5px;
+        .title{
+          display: flex;
+          width: 40%;
+          height: 100%;
+          color: white;
+          justify-content: center;
+          align-items: center;
+          font-size: 22px;
+          font-weight: 700;
+          letter-spacing: 5px;
+        }
+      }                                       
+    }
   }
 }
 </style>
