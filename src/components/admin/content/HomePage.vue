48e38cd77bc21a5a55f695231947b769f8f477bd
chenchen.xu <chenxu2656@163.com>
2022-05-22 18:22:48 +0800

    update

--- src/components/admin/content/HomePage.vue
+++ src/components/admin/content/HomePage.vue
@@ -14,6 +14,28 @@
       <el-col :span="6"><div class="grid-content bg-purple" /></el-col>
     </el-row>
   </div>
+  <div id="con">
+    <el-row :gutter="20">
+      <el-col :span="8">
+        <div class="cont" id="latestBlog">
+          <div class="tit">
+            <div class="titCon">近期文章</div>
+            <div class="readMore">阅读更多</div>
+          </div>
+        </div>
+      </el-col>
+<el-col :span="8">
+      <div class="cont" id="pinglun">
+
+        </div>
+      </el-col>
+      <el-col :span="8">
+        <div class="cont" id="latestBlog">
+
+        </div>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 <style lang="scss" scoped>
 #headBar {
@@ -36,6 +58,18 @@
           letter-spacing: 5px;
         }
       }                                       
+    }
+  }
+}
+#con{
+  margin-top: 20px;
+  .cont{
+    height: 500px;
+    border-radius: 5px;
+    .tit{
+      .titCon{
+        
+      }
     }
   }
 }
