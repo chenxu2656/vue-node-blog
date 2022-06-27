5f1ca153f819ebaa646abf11e577e61304625106
chenxu2656 <chenxu2656@gmail.com>
2022-06-27 22:17:19 +0800

    fix ui issues

--- src/components/admin/content/systemSetting/UserInfo.vue
+++ src/components/admin/content/systemSetting/UserInfo.vue
@@ -1,52 +1,44 @@
 <template>
     <el-card>
-        123
         <div id="avatar">
-            <el-upload id="upload" class="avatar-uploader" drag 
-            :show-file-list="false"
-            >
+            <el-upload id="upload" class="avatar-uploader" :show-file-list="false">
                 <img v-if="imageUrl" :src="imageUrl" class="avatar" />
-    <el-icon v-else class="avatar-uploader-icon"><Plus /></el-icon>
+                <el-icon v-else class="avatar-uploader-icon">
+                    <Plus />
+                </el-icon>
             </el-upload>
         </div>
     </el-card>
-
 </template>
-
 <script setup>
 import { ref } from 'vue'
 import { Plus } from '@element-plus/icons-vue'
-
-
 const imageUrl = ref('')
-
-
 </script>
-
 <style lang="scss" scoped>
 .avatar-uploader .avatar {
-  width: 178px;
-  height: 178px;
-  display: block;
+    width: 100px;
+    height: 100px;
+    display: block;
 }
 .avatar-uploader .el-upload {
-  border: 1px dashed var(--el-border-color);
-  border-radius: 6px;
-  cursor: pointer;
-  position: relative;
-  overflow: hidden;
-  transition: var(--el-transition-duration-fast);
+    width: 100px;
+    height: 100px;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
 }
-
 .avatar-uploader .el-upload:hover {
-  border-color: var(--el-color-primary);
+    border-color: gray;
 }
-
 .el-icon.avatar-uploader-icon {
-  font-size: 28px;
-  color: #8c939d;
-  width: 178px;
-  height: 178px;
-  text-align: center;
+    font-size: 28px;
+    color: #8c939d;
+    width: 100px;
+    height: 100px;
+    text-align: center;
+    border: 1px dashed black;
+    border-radius: 50%;
 }
 </style>
